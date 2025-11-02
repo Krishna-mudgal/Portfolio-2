bd34b270e87920a9778988d5d22ed0f9152772d9
Krishna-mudgal <tonystark00127@gmail.com>
2025-11-02 21:40:09 +0530

    fix some bugs

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -12,8 +12,8 @@ import javascript from './../assets/javaScript.svg';
 import reactIcon from './../assets/react.svg';
 import tailwind from './../assets/tailwind.svg';
 import github from './../assets/gitHub.svg';
-import node from "./../assets/nodejsStackedDArk.svg";
-import express from "./../assets/express.png"
+import nodejs from "./../assets/nodejsStackedDark.svg";
+import expressjs from "./../assets/express.png"
 
 import problemSolving from './../assets/problemSolving.png';
 import projectManagement from './../assets/projectManagement.png';
@@ -94,8 +94,8 @@ const TechStack = () => {
           <SkillCard icon={reactIcon} label="React" index={4} />
           <SkillCard icon={tailwind} label="TailwindCSS" index={5} />
           <SkillCard icon={github} label="GitHub" index={6} />
-          <SkillCard icon={node} label="Node.js" index={7} />
-          <SkillCard icon={express} label="Express" index={8} />
+          <SkillCard icon={nodejs} label="Node.js" index={7} />
+          <SkillCard icon={expressjs} label="Express" index={8} />
         </div>
       </motion.section>
 
